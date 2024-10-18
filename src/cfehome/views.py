f7f8bac7d5d0425f591f4fc17b1f2786eb8a25e8
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2024-10-18 20:59:17 +0530

    This is the first commit

---
+++ src/cfehome/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+import pathlib
+from django.shortcuts import render
+from visits.models import PageVisit
+
+this_dir=pathlib.Path(__file__).resolve().parent
+def home_page_view(request,*args,**kwargs):
+  qs=PageVisit.objects.all()
+  page_qs=PageVisit.objects.filter(path=request.path)
+  my_title='My page'
+  my_context={
+    'page_title':my_title,
+    'totalvisits':qs.count(),
+    'percent':page_qs.count()*100/qs.count(),
+    'page_qs':page_qs.count()
+  }
+  path=request.path
+  html_template='home.html'
+  PageVisit.objects.create(path=request.path)
+  return render(request,html_template,my_context)
+
